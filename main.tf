1c77d84c2a4ac783d9b7c432629d060eef5e7c52
Prasanth Kommini <prashanth.kommini@gmail.com>
2021-11-23 02:41:53 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -111,11 +111,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_eip" "hashicat" {
   instance = aws_instance.hashicat.id
   vpc      = true
-
-  tags = {
-    Department = "devops"
-    Billable = "true"
-  }
 }
 
 resource "aws_eip_association" "hashicat" {
@@ -133,6 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    Department = "devops"
+    Billable = "true"
   }
 }
 
